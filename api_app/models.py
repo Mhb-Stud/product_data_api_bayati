52b09a49881d89680db24fe873b90b3c5a3477e4
mhb_dev <bayatim81@gmail.com>
2021-11-15 11:45:30 +0330

    Repositioned a Comment in Models.py

--- api_app/models.py
+++ api_app/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
+
 # a model for person to be added as a table to the database by django
 # for creating models your model class in this case person should inherit
 # from Model class in django and after that dont forget to migrate
-
-
 class Person(models.Model):
     first_name = models.TextField()
     last_name = models.TextField()
