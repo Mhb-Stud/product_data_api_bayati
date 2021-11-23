6e3b8327b030cb737304c4b41ce163ec58c0738e
mhb_dev <bayatim81@gmail.com>
2021-11-23 13:22:07 +0330

    Changed database structure now it is normal and queries can run optimal

--- gateway/models.py
+++ gateway/models.py
@@ -1,15 +1,29 @@
 from django.db import models
 
 
-""" 
- for creating models your model class in this case person should inherit
- from Model class in django and after that dont forget to migrate
-"""
+class Vendor(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'vendor'
+
+
+class User(models.Model):
+    username = models.CharField(primary_key=True, max_length=50)
+    password = models.TextField()
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+
+    """ 
+     for creating models your model class in this case person should inherit
+     from Model class in django and after that dont forget to migrate
+    """
 class Product(models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=50)
     price = models.IntegerField(default=0)
     base_price = models.IntegerField(default=0)
+    vendor = models.ForeignKey(Vendor, max_length=50)
 
     class Meta:
         db_table = 'product'
@@ -17,21 +31,5 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
-class Vendor(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=50)
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
 
-    class Meta:
-        db_table = 'vendor'
-
-
-class ProductInstance:
-    id = models.IntegerField(primary_key=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'product_instance'
 
