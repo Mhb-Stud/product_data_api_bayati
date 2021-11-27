bd7c56711cf9aa1f79191ea5e86f73eefea5e54c
mhb_dev <bayatim81@gmail.com>
2021-11-27 21:17:42 +0330

    Changed user model's password field from charfield into textfield

--- gateway/models.py
+++ gateway/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     this model holds user data and overrides django default user also changed settings.py for config
     """
     username = models.CharField(primary_key=True, max_length=50)
-    password = models.CharField(max_length=300)
+    password = models.TextField()
     email = models.EmailField(blank=True)
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
