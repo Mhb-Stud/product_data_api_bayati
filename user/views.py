8e630b2f661075db250896e198f27b1c6847ef7a
mhb_dev <bayatim81@gmail.com>
2021-12-11 10:43:50 +0330

    Changed return query for user.views

--- user/views.py
+++ user/views.py
@@ -19,9 +19,10 @@ class UserHandler(viewsets.ViewSet):
     permission_classes = [IsAuthenticated, ]
 
     def list(self, request):
-        data = VendorProduct.objects.filter(vendor__name=request.user.username)
-        serialized = VendorProductSerializer(data, many=True)
-        return Response(serialized.data)
+        data = VendorProduct.objects.values('product__id', 'base_price', 'price', 'discount_percent',
+                                            'discount_price_difference', 'number_of_views', 'product__title',
+                                            'product__brand__name', 'product__category__name').filter(vendor__name=request.user.username)
+        return Response(data)
 
 
 class RegisterView(viewsets.ViewSet):
