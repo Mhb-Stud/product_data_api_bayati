f9ccda78434c980dca46fa4e7b6dcc7830de9a4d
mhb_dev <bayatim81@gmail.com>
2021-12-17 11:55:35 +0330

    Added exception handling to photo downloader

--- user/tasks.py
+++ user/tasks.py
@@ -7,6 +7,10 @@ import wget
 @shared_task
 def download_vendor_photo(photo_url, username):
     response = wget.download(photo_url, out='media/vendor/images')
-    my_object = Vendor.objects.get(name=username)
+    my_object = None
+    try:
+        my_object = Vendor.objects.get(name=username)
+    except Vendor.DoesNotExist:
+        print("user is not a vendor!")
     my_object.logo = response
     my_object.save()
