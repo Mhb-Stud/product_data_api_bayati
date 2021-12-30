43651a28268269b05776fc90dad8c443db9969cc
mhb_dev <bayatim81@gmail.com>
2021-12-30 18:22:16 +0330

    Changed name of the command Command now works it reads images and sends them to vendor model and then the models decides how to save the image

--- shop/management/commands/vendor_image.py
+++ shop/management/commands/vendor_image.py
@@ -1,31 +1,36 @@
 from django.core.management.base import BaseCommand
 from django.core.files import File
 import os
+from io import BytesIO
 from config.settings import BASE_DIR
-from shop.models import Category
+from shop.models import Vendor
 from PIL import Image
 from django.db.models.fields.files import ImageFieldFile, FileField
-import wget
-from shop.models import Vendor
 
 class Command(BaseCommand):
-    help = 'gets and copies vendor images to db'
+    help = 'Displays current time'
 
     def handle(self, *args, **kwargs):
-        file_path = os.path.join(BASE_DIR, 'shop/management/commands/vendor_image_link.txt')
-        with open(file_path, 'r') as file:
-            while file:
-                url = file.readline().replace('\n', '')
-                vendor_name = file.readline().replace('\n', '')
-                file_name = vendor_name + os.path.splitext(url)[-1]
-                self.download_vendor_photo_and_save(url, vendor_name, file_name)
+        self.get_category_image_files()
 
-    def download_vendor_photo_and_save(self, photo_url, vendor_name, file_name):
-        store_path = f'media/vendor_logos/{file_name}'
-        response = wget.download(photo_url, out=store_path)
+    def get_category_image_files(self):
+        folder_path = os.path.join(BASE_DIR, 'media/vendor_logos')
+        image_paths = [os.path.join(folder_path, name) for name in os.listdir(folder_path)]
+        for path in image_paths:
+            with open(path, 'rb') as file:
+                image = file.read()
+            memory_file = BytesIO(image)
+            extension = os.path.splitext(path)[-1]
+            file_name = os.path.splitext(path)[0].rsplit('\\')[-1] + extension
+            vendor_name = os.path.splitext(path)[0].rsplit('\\')[-1]
+            to_be_saved = File(memory_file, name=file_name)
+            self.save_to_database(vendor_name=vendor_name, to_be_saved=to_be_saved)
+
+    def save_to_database(self, vendor_name, to_be_saved):
         try:
-            my_object = Vendor.objects.get(name=vendor_name)
-            my_object.logo = response
-            my_object.save()
+            vendor = Vendor.objects.get(name=vendor_name)
+            if vendor.logo is None:
+                vendor.logo = to_be_saved
+                vendor.save()
         except Vendor.DoesNotExist:
-            Vendor.objects.create(name=vendor_name, logo=response)
+            Vendor.objects.create(name=vendor_name, logo=to_be_saved)
